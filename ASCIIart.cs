bb84c59aae953c5efe9234fc8e009e3dc5b3f84d
CalebTek <walexjackson@gmail.com>
2023-04-25 17:38:34 +0100

    Update ASCIIart.cs

--- ASCIIart.cs
+++ ASCIIart.cs
@@ -5,11 +5,31 @@ Console.WriteLine();
 
 int width = 50, height = 20;
 
+Console.WriteLine("SOLID RECTANGLE");
 for (int i = 0; i < height; i++)
 {
     for (int j = 0; j < width; j++)
     {
         Console.Write("#");
+    }
+    Console.WriteLine();
+}
+
+
+Console.WriteLine("BORDER RECTANGLE");
+int borderWidth = 2;
+for (int i = 0; i < height; i++)
+{
+    for (int j = 0; j < width; j++)
+    {
+        if (i < borderWidth || i > height -1 - borderWidth 
+            || j < borderWidth || j > height - 1 - borderWidth)
+            {
+               Console.Write("#"); 
+            } else
+            {
+                Console.Write(" ");
+            }
     }
     Console.WriteLine();
 }
