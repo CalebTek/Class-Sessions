8e4f20122cb83b810d8d620cbe64090838415ac4
CalebTek <walexjackson@gmail.com>
2023-04-25 19:33:19 +0100

    correct len error

--- arrayTest.cs
+++ arrayTest.cs
@@ -3,14 +3,14 @@ using System;
 int arrSize = 10;
 int[] numArr = new int[arrSize];
 int counter = 0;
-for (int i = 1; i < len*3; i++)
+for (int i = 1; i < arrSize*3; i++)
 {
     if (i%2 == 0)
     {
         //numbers.Add(i);
         numArr[i-1] = i;
         counter ++;
-        if (counter >= len)
+        if (counter >= arrSize)
         {
             break;
         }
