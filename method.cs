c8b23cc8bfc2f461caf50c1c15ce7c7df2ba5bca
CalebTek <walexjackson@gmail.com>
2023-04-25 18:25:49 +0100

    Update method.cs

---
+++ method.cs
@@ -0,0 +1,42 @@
+using System;
+
+/*
+Program that print even number within a given range of number
+*/
+Console.Write("How many numbers do you want to print: ");
+int length;
+
+try
+{
+    length = Convert.ToInt32(Console.ReadLine());
+}
+catch (System.Exception)
+{
+    Console.WriteLine("Input is wrong, Please enter a number");
+    throw;
+}
+
+
+// Even Numbers
+// Using modulo operator
+//Console.WriteLine($"Print {length} even number(s) using modulo operator ");
+
+Static int EvenNumbers(int length)
+{
+    List<int> numbers = new List<int>();
+    int counter = 0 ;
+    for (int i = 1; i < length*3; i++)
+    {
+        if (i%2 == 0)
+        {
+            numbers.Add(10*i);
+            counter ++;
+            if (counter >= length)
+            {
+                break;
+            }
+        }
+        
+    }
+    return numbers
+}
