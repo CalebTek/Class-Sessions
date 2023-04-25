c10dbe05e5c6cbaa57b8fefe82057afdc90cc789
CalebTek <walexjackson@gmail.com>
2023-04-25 19:30:20 +0100

    create an array test file

---
+++ arrayTest.cs
@@ -0,0 +1,19 @@
+using System;
+
+int arrSize = len;
+int[] numArr = new int[arrSize];
+int counter = 0;
+for (int i = 1; i < len*3; i++)
+{
+    if (i%2 == 0)
+    {
+        //numbers.Add(i);
+        numArr[i-1] = i;
+        counter ++;
+        if (counter >= len)
+        {
+            break;
+        }
+    }
+    
+}
