d1c0bd3a433f115527ddd98da749c3fc6e0d5895
CalebTek <walexjackson@gmail.com>
2023-04-25 17:32:14 +0100

    Update ASCIIart.cs

--- ASCIIart.cs
+++ ASCIIart.cs
@@ -1,1 +1,15 @@
 using System;
+
+Console.WriteLine("Let's create some cool ASCII art!");
+Console.WriteLine();
+
+int width = 50, height = 20;
+
+for (int i = 0; i < height; i++)
+{
+    for (int j = 0; j < width; j++)
+    {
+        Console.Write("#");
+    }
+    Console.WriteLine();
+}
