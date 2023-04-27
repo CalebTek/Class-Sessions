90a3945ade1038bfac8f4b82264700eddf55816d
CalebTek <walexjackson@gmail.com>
2023-04-27 16:11:40 +0100

    Bubble sort updates

--- bubbleSort.cs
+++ bubbleSort.cs
@@ -20,16 +20,17 @@ namespace Sorting
             int temp;
             for (int j = 0; j <= arr.Length - 2; j++) 
             {
-            for (int i = 0; i <= arr.Length - 2; i++) 
-            {
-               if (arr[i] > arr[i + 1]) 
-               {
-                  temp= arr[i + 1];
-                  arr[i + 1] = arr[i];
-                  arr[i] = temp;
-               }
+                for (int i = 0; i <= arr.Length - 2; i++) 
+                {
+                    if (arr[i] > arr[i + 1]) 
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
             }
-            }
+            return arr;
         }
     }
 }
