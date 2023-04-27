e6d487df8a3b3a80ff94d3b7ced76471f7239d46
CalebTek <walexjackson@gmail.com>
2023-04-27 15:52:23 +0100

    Update bubbleSort.cs

--- bubbleSort.cs
+++ bubbleSort.cs
@@ -6,37 +6,30 @@ namespace Sorting
     {
         static void Main(string[] args)
         {
-
+            int[] arr = { 78, 55, 45, 98, 13 };
+            Console.WriteLine("Sorted:");
+            int[] sorted = BubbleSort(arr);
+            foreach (int p in sorted)
+            {
+                Console.Write(p + " ");
+            }   
         }
 
         static int[] BubbleSort(int[] arr)
         {
-            
-        }
-    }
-}
-
-
-
-
-namespace BubbleSort {
-   class MySort {
-      static void Main(string[] args) {
-         int[] arr = { 78, 55, 45, 98, 13 };
-         int temp;
-         for (int j = 0; j <= arr.Length - 2; j++) {
-            for (int i = 0; i <= arr.Length - 2; i++) {
-               if (arr[i] > arr[i + 1]) {
+            int temp;
+            for (int j = 0; j <= arr.Length - 2; j++) 
+            {
+            for (int i = 0; i <= arr.Length - 2; i++) 
+            {
+               if (arr[i] > arr[i + 1]) 
+               {
                   temp= arr[i + 1];
                   arr[i + 1] = arr[i];
                   arr[i] = temp;
                }
             }
-         }
-         Console.WriteLine("Sorted:");
-         foreach (int p in arr)
-            Console.Write(p + " ");
-         Console.Read();
-      }
-   }
+            }
+        }
+    }
 }
