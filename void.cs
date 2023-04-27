70b75b9f9edf12a6fd983b84a65493c60a2bae6f
CalebTek <walexjackson@gmail.com>
2023-04-27 08:30:23 +0100

    Update void.cs

---
+++ void.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace testVoid
+{
+    class VoidAction
+    {
+        static void Main(string[] args)
+        {
+            string name = "C.E.O Odeyemi"
+            Log($"{name}");
+        }
+
+        static void Log(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
