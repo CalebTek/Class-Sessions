4654e4c809b1b1a96a35ac512d8073064b5817a8
CalebTek <walexjackson@gmail.com>
2023-04-28 20:19:33 +0100

    Create int32to string method

--- binary.cs
+++ binary.cs
@@ -6,7 +6,20 @@ namespace Binary
     {
         static void Main(string[] args)
         {
-            
+
+        }
+
+        public static string Int32ToString(int value, int toBase)
+        {
+            string result = string.Empty;
+            do
+            {
+                result = "0123456789ABCDEF"[value % toBase] + result;
+                value /= toBase;
+            }
+            while (value > 0);
+
+            return result;
         }
     }
 }
