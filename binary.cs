adcabdd74bedafc2eb224888bbce53bcad7ef620
CalebTek <walexjackson@gmail.com>
2023-04-28 21:29:52 +0100

    updates binary

--- binary.cs
+++ binary.cs
@@ -6,7 +6,9 @@ namespace Binary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Int32ToString(12345,2));
+            Console.WriteLine(Int32ToString(1234,2));
+            Console.WriteLine(CountBits(1234));
+            Console.WriteLine(CountBits2(1234));
         }
 
         public static string Int32ToString(int value, int toBase)
@@ -19,7 +21,31 @@ namespace Binary
             }
             while (value > 0);
 
+            
+
             return result;
+        }
+
+        public static int CountBits(int n)
+        {
+            int count = 0;
+            while (n != 0)
+            {
+                count += n & 1;
+                n >>= 1;
+            }
+         return count;
+        }
+
+        public static int CountBits2(int n)
+        {
+            int count = 0;
+            while (n > 0)
+            {
+                count += n % 2;
+                n /= 2;
+            }
+         return count;
         }
     }
 }
