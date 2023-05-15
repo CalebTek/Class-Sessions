3197038b93d8a16623dab8641ce2c52c60183ade
CalebTek <walexjackson@gmail.com>
2023-05-15 01:13:13 +0100

    Unique Numbers

--- Mosh Exercises/Csharp/Array--List/Exercise-3.cs
+++ Mosh Exercises/Csharp/Array--List/Exercise-3.cs
@@ -4,3 +4,38 @@ If a number has been previously entered, display an error message and ask the us
 Once the user successfully enters 5 unique numbers, sort them and display the result on the console.
 */
 
+using System;
+using System.Collections.Generic;
+
+namespace Exercise_3
+{
+    class UniqueNumbers
+    {
+        static void Main(string[] args)
+        {
+            var numberList = new List<int>();
+            int start = 0, end = 5;
+            do
+            {
+                Console.Write("Enter a number: ");
+                var input = Console.ReadLine();
+                int number;
+                while (!int.TryParse(input, out number) || numberList.Contains(number))
+                {
+                    Console.Write("Invalid Input, please re-try: ");
+                    input = Console.ReadLine();
+                }
+                start++;
+                numberList.Add(number);
+            } while (start < end);
+            numberList.Sort();
+            foreach (var number in numberList)
+            {
+                Console.Write($"{number} ");
+            }
+
+        }
+
+        
+    }
+}
--- Mosh Exercises/Csharp/Array--List/Exercise-4.cs
+++ Mosh Exercises/Csharp/Array--List/Exercise-4.cs
@@ -3,3 +3,43 @@
 The list of numbers may include duplicates. Display the unique numbers that the user has entered.
 */
 
+
+using System;
+using System.Collections.Generic;
+
+namespace Exercise_3
+{
+    class UniqueNumbers
+    {
+        static void Main(string[] args)
+        {
+            var numberList = new List<int>();
+            do
+            {
+                Console.Write("Enter a number: ");
+                var input = Console.ReadLine();
+                if (input.ToUpper() == "QUIT")
+                {
+                    break;
+                }
+                int number;
+                while (!int.TryParse(input, out number))
+                {
+                    Console.Write("Invalid Input, please re-try: ");
+                    input = Console.ReadLine();
+                }
+                if (!numberList.Contains(number))
+                {
+                    numberList.Add(number);
+                }
+            } while (true);
+            foreach (var number in numberList)
+            {
+                Console.Write($"{number} ");
+            }
+
+        }
+
+
+    }
+}
