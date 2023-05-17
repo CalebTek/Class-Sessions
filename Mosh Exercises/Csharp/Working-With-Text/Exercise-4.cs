75f6cb88cca1859f10c02ee1cfccf2afcb062e43
CalebTek <walexjackson@gmail.com>
2023-05-18 00:49:44 +0100

    Check vowel

--- Mosh Exercises/Csharp/Working-With-Text/Exercise-4.cs
+++ Mosh Exercises/Csharp/Working-With-Text/Exercise-4.cs
@@ -5,3 +5,24 @@ For example, if the user types: "number of students", display "NumberOfStudents"
 Make sure that the program is not dependent on the input. 
 So, if the user types "NUMBER OF STUDENTS", the program should still display "NumberOfStudents".
 */
+
+using System;
+using System.Text;
+using System.Linq;
+namespace Exercise_4
+{
+    class PascalCasePhrase
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a few words separated by a space: ");
+            var phrase = Console.ReadLine();
+            var phraseArray = phrase.ToLower().Split();
+            var titleCase = phraseArray.Select(i => Char.ToUpper(i[0]) + i.Substring(1));
+            var pascalCase = String.Join("", titleCase);
+            //var pascalCase = String.Join(" ", phrase.ToLower().Split().Select(i => Char.ToUpper(i[0]) + i.Substring(1))).Replace(" ","");
+            Console.WriteLine(pascalCase);
+        }
+
+    }
+}
--- Mosh Exercises/Csharp/Working-With-Text/Exercise-5.cs
+++ Mosh Exercises/Csharp/Working-With-Text/Exercise-5.cs
@@ -4,3 +4,18 @@ Count the number of vowels (a, e, o, u, i) in the word.
 So, if the user enters "inadequate", the program should display 6 on the console.
 */
 
+using System;
+using System.Linq;
+namespace Exercise_5
+{
+    class vowels
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an English word: ");
+            var word = Console.ReadLine();
+            var vowelCount = word.Count(x => x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u');
+            Console.WriteLine($"Vowels in {word} = {vowelCount}");
+        }
+    }
+}
